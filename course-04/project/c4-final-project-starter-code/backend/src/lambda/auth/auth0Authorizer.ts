db3e8f27f56ce214e08140bf15c3398938d3f8e6
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-05-17 10:22:51 +0530

    WIP: Integrating get todos api

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,10 +9,7 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-j8omys-t.auth0.com/pem'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +55,19 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  if(!jwt){
+    throw new Error('invalid token')
+  }
+
+  try {
+    const response = await Axios.get(jwksUrl);
+    var verifedToken = verify(token,response.data,{algorithms:['RS256']})
+
+    return  verifedToken as JwtPayload
+  } catch (error) {
+    logger.error(error);
+    return undefined
+  }
 }
 
 function getToken(authHeader: string): string {
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,50 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todoTable = process.env.TODO_TABLE
+const todoTableIndex = process.env.INDEX_NAME
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  console.log('Processing event: ', event)
+  const userId = '1'
+
+  const params = {
+    TableName: todoTable,
+    IndexName: todoTableIndex,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
+  };
+
+  const result = await docClient.query(params).promise();
+
+  if(result.Count !== 0){
+    return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(
+            result.Items[0]
+        )
+      }
+  }  
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+        'error': 'No comment with this id'
+    })
+  }
+
+
 }
