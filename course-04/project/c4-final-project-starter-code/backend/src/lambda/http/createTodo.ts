8cc95fb9e8d1dc25497e4902a69c35052dd2476e
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-05-18 08:53:44 +0530

    Added method to update todo item

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -48,5 +48,4 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
           newPost: todoItem
       })
     }
-
 }
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,42 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+//import { TodoItem } from '../../models/TodoItem'
 
-// //import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   // const todoId = event.pathParameters.todoId
-//   // const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-//   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-//   return undefined
-// }
+  var params = {
+    TableName:todoTable,
+    Key:{
+        "todoId": todoId,
+    },
+    UpdateExpression: "set #namefield = :n, dueDate = :d, done = :done",
+    ExpressionAttributeValues:{
+        ":n":updatedTodo.name,
+        ":d":updatedTodo.dueDate,
+        ":done":updatedTodo.done
+    },
+    ExpressionAttributeNames:{
+        "#namefield": "name"
+    }
+}
+
+await docClient.update(params).promise();
+
+return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: "Updated Succesfully"
+  } 
+}
+
+
